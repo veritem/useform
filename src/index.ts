9991b109fed848759fb7c49e1b189408c8ea80ce
Verité Mugabo <mugaboverite@gmail.com>
2023-01-11 22:31:44 +0200

    feat: updated deps and error with handlechange func (#4)

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,42 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function useForm<T>(initial: T) {
-  const [inputs, setInputs] = useState<T>(initial);
-  const initialValues = Object.values(initial).join("");
+	const [inputs, setInputs] = useState<T>(initial);
+	const initialValues = Object.values(initial).join("");
 
-  useEffect(() => {
-    setInputs(initial);
-  }, [initialValues]);
+	useEffect(() => {
+		setInputs(initial);
+	}, [initialValues]);
 
-  function handleChange(e) {
-    let { value, name, type } = e.target;
-    if (type === "number") {
-      value = parseInt(value);
-    }
-    if (type === "file") {
-      value = e.target.files;
-    }
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  }
+	function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+		let { value, name, type } = e.target;
+		if (type === "number") {
+			value = parseInt(value);
+		}
+		if (type === "file") {
+			value = e.target.files;
+		}
+		setInputs({
+			...inputs,
+			[name]: value,
+		});
+	}
 
-  function resetForm() {
-    setInputs(initial);
-  }
+	function resetForm() {
+		setInputs(initial);
+	}
 
-  function clearForm() {
-    const blankState = Object.fromEntries(
-      Object.entries(inputs).map(([key, value]) => [key, ""])
-    ) as unknown as T;
-    setInputs(blankState);
-  }
+	function clearForm() {
+		const blankState = Object.fromEntries(
+			Object.entries(inputs).map(([key, value]) => [key, ""])
+		) as unknown as T;
+		setInputs(blankState);
+	}
 
-  return {
-    inputs,
-    handleChange,
-    resetForm,
-    clearForm,
-  };
+	return {
+		inputs,
+		handleChange,
+		resetForm,
+		clearForm,
+	};
 }
